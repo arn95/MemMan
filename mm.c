3284ee0e9b4cb5ddd73a64af3a18d85c7c0a43be
Arnold Balliu <arnold.balliu@gmail.com>
2017-03-29 07:00:55 -0400

    final version

--- mm.c
+++ mm.c
@@ -50,9 +50,51 @@ const int NO_FREE_SPACE = 33;
 const int ALL_FREE_SPACE_FILLED = 22;
 const int FREE_SPACE_REMAINING = 11;
 
+const int NEED_NEW_BLOCK = 123;
+const int LARGEST_BLOCK_FITS = 9866;
+
+typedef struct header* Header;
+
 struct free_list_s {
     Header head;
 } FreeList;
+
+// Custom function declaration
+
+/** requests page-aligned memory **/
+void* fl_alloc(size_t pasize);
+
+/** deallocates page-aligned memory **/
+void fl_dealloc();
+
+/** Splits the block into to be used and free memory
+ * returns: remaining free mem
+ * pass-by-ref:
+ *      status = (NO_FREE_SPACE | ALL_FREE_SPACE_FILLED | FREE_SPACE_REMAINING)
+ *      points_where = a reference to where the original block pointed
+ * **/
+Header fl_split_block(void*, size_t , size_t, int*, void**);
+
+/** embeds a header to the page-aligned memory **/
+Header fl_new_block(size_t);
+
+/** coalesces the blocks in the free list **/
+void fl_coalesce();
+
+/** merges two headers together if they're eligible **/
+Header fl_merge_headers(Header, Header);
+
+/** approriately connects the eligible headers together with the rest of the list **/
+Header fl_connect_headers(Header, Header, Header, Header);
+
+/** applies mergesort recursively on free list, sorting it in descending order of free space size **/
+void fl_mergesort(Header*);
+
+/** merges 2 linked list halves **/
+Header fl_mergesort_merge(Header, Header);
+
+/** splits the given linked list into 2 halves **/
+void fl_mergesort_split(Header, Header*, Header*);
 
 /*
  * mm_init - initialize the malloc package.
@@ -60,8 +102,7 @@ struct free_list_s {
 int mm_init(void)
 {
     // no pages allocated yet.
-    FreeList.head = NULL;
-
+    FreeList.head = NULL; // head is also largest block
     return 0;
 }
 
@@ -69,81 +110,119 @@ int mm_init(void)
  * mm_malloc - Allocate a block by using bytes from current_avail,
  *     grabbing a new page if necessary.
  */
-void *mm_malloc(size_t size)
-{
+void *mm_malloc(size_t size) {
 
-    size_t asize = ALIGN(size+HEAD_SIZE); //allocation needed 
-    size_t pasize = PAGE_ALIGN(asize); //page allocation given
-
-    if (size <= 0) {
+    if (size <= 0) { // cant request negative size
         return NULL;
     }
 
-    if (FreeList.head == NULL) {
-        Header block = new_block(pasize);
+    size_t asize = ALIGN(size+HEAD_SIZE);
+    size_t pasize = PAGE_ALIGN(asize);
+
+    if (FreeList.head == NULL) { // list is empty
+        Header block = fl_new_block(pasize);
 
         if (block == NULL) {
             return NULL;
         }
 
         int status = 0;
-        Header remainder = split(block, pasize,asize,&status, NULL);
+        Header remainder = fl_split_block(block,pasize,asize,&status, NULL);
         if (status == FREE_SPACE_REMAINING) {
             FreeList.head = remainder;
+            FreeList.head->next = NULL;
+        } else if (status == ALL_FREE_SPACE_FILLED){
+            FreeList.head = NULL;
         }
 
         return ((void*)block+HEAD_SIZE);
 
-    } else {
-        void* who_points = NULL;
-        Header block = find_block(&who_points,asize);
+    } else { //list has at least 1 header
+        int situation = 0;
+        int status = 0;
+        void* curr_pointer_next = NULL;
+        Header block = NULL;
 
-        if (block == NULL) { // No block big enough to fit
-            block = new_block(pasize);
-            if (block == NULL) {
-                return NULL;
-            }
-            int status = 0;
-            void* points_where = NULL;
-            Header remainder = split(block, pasize, asize, &status, &points_where);
-            if (status == FREE_SPACE_REMAINING){
-                remainder->next = FreeList.head;
-                FreeList.head = remainder;
-            }
-            return  ((void*)block+HEAD_SIZE);
-        } else { // Found block that can fit
+        if (FreeList.head->size >= asize){
+            situation = LARGEST_BLOCK_FITS;
+        } else {
+            situation = NEED_NEW_BLOCK;
+        }
 
-            int status = 0;
-            void* points_where = NULL;
-            Header remainder = split(block, block->size, asize, &status, &points_where);
-            if (status == FREE_SPACE_REMAINING){
-                if (who_points != NULL) {
-                    ((Header)who_points)->next = remainder;
-                } else {
+        switch(situation){
+            case LARGEST_BLOCK_FITS : {
+
+                block = (void*)FreeList.head; //largest block
+
+                Header remainder = fl_split_block(FreeList.head, FreeList.head->size, asize, &status , &curr_pointer_next);
+                if (FREE_SPACE_REMAINING == status){
+                    FreeList.head = remainder;
+                    remainder->next = ((Header)curr_pointer_next);
+                } else if (ALL_FREE_SPACE_FILLED == status){
+                    FreeList.head = ((Header)curr_pointer_next);
+                }
+
+                return ((void*)block+HEAD_SIZE);
+
+            } //break;
+            case NEED_NEW_BLOCK : {
+
+                block = fl_new_block(pasize);
+                if (block == NULL) {
+                    return NULL;
+                }
+
+                Header remainder = fl_split_block(block, pasize, asize, &status, NULL);
+                if (status == FREE_SPACE_REMAINING){ //always inserted in the beginning of the list
+                    remainder->next = FreeList.head;
                     FreeList.head = remainder;
                 }
-                remainder->next = ((Header)points_where);
-            } else if (status == ALL_FREE_SPACE_FILLED){
-                if (who_points != NULL){
-                    ((Header)who_points)->next = ((Header)points_where);
-                } else {
-                    FreeList.head = ((Header)points_where);
-                }
-            }
 
-            return ((void*)block+HEAD_SIZE);
+                return ((void*)block+HEAD_SIZE);
+            } //break;
+
+            default: {
+                return NULL;
+            }
         }
     }
 }
 
-Header merge(Header h1, Header h2){
+/*
+ * mm_free - Freeing a block does nothing.
+ */
+void mm_free(void* ptr) {
+    /* You must implement this. If a page is completely free, use
+     * mem_unmap() from driver/memlib.c to return it to the OS.
+     */
+
+    if (ptr == NULL) {
+        return;
+    }
+
+    Header ptr_h = ptr-HEAD_SIZE;
+    if (ptr_h == NULL)
+        return;
+
+    if (ptr_h->magic_number == 123456){
+        ptr_h->next = FreeList.head;
+        FreeList.head = ptr_h;
+        fl_coalesce();
+        fl_dealloc();
+        fl_mergesort(&FreeList.head);
+    } else {
+        return;
+    }
+}
+
+Header fl_merge_headers(Header h1, Header h2){
 
     Header merged = NULL;
 
     if (h1 == NULL || h2 == NULL)
         return NULL;
 
-    if ( ((void*)h1+h1->size) == h2 ) {
+    if ( ((void*)h1+(h1->size+HEAD_SIZE)) == h2 ) {
         merged = h1;
         merged->size = h1->size + h2->size;
     }
@@ -151,51 +230,41 @@ Header merge(Header h1, Header h2){
     return merged;
 }
 
-Header merge_connect(Header prev, Header curr, Header prev_other, Header other){
+Header fl_connect_headers(Header prev, Header curr, Header prev_other, Header other) {
+
     Header merged = NULL;
-    merged = merge(curr, other);
+    merged = fl_merge_headers(curr, other); //merging on curr
     if (merged != NULL){
-        if (prev == NULL){
-            if (prev_other == NULL){
-                FreeList.head = merged;
-                merged->next = other->next;
-            } else {
-                prev_other->next = other->next;
-            }
+        if (prev_other == NULL){
+            curr->next = other->next;
         } else {
-            if (prev_other == NULL){
-                merged->next = other->next;
-            } else {
-                prev_other->next = other->next;
-            }
+            prev_other->next = other->next;
         }
+        return merged;
     }
-    merged = merge(other, curr);
+
+    merged = fl_merge_headers(other, curr); //merging on other
     if (merged != NULL){
         if (prev == NULL){
             if (prev_other == NULL){
-                FreeList.head = merged;
+                FreeList.head = other;
             } else {
                 FreeList.head = curr->next;
-                merged->next = other->next;
             }
         } else {
             if (prev_other == NULL){
-                if (other->next == NULL){
-                    prev->next = merged;
-                } else {
-                    prev->next = merged;
-                    merged->next = other->next;
-                }
+                prev->next = other;
             } else {
                 prev->next = curr->next;
             }
         }
+        return merged;
     }
-    return merged;
+
+    return NULL;
 }
 
-void merge_terse(){
+void fl_coalesce(){
     Header curr = FreeList.head;
     Header prev = NULL;
     Header other = NULL;
@@ -204,11 +273,11 @@ void merge_terse(){
     if (curr == NULL)
         return;
 
-    while (curr->next != NULL){
+    while (curr != NULL){
         other = curr->next;
         while (other != NULL){
 
-            merge_connect(prev, curr, prev_other, other);
+            fl_connect_headers(prev, curr, prev_other, other);
 
             prev_other = other;
             other = other->next;
@@ -220,29 +289,9 @@ void merge_terse(){
     }
 }
 
-Header find_block(void** who_points, size_t asize){
-    Header curr = FreeList.head;
-    Header prev = NULL;
+Header fl_new_block(size_t pasize){
 
-    if (FreeList.head == NULL)
-        return NULL;
-
-    while (curr->next != NULL) {
-        if (curr->size >= asize)
-            return curr;
-        prev = curr;
-        curr = curr->next;
-    }
-    *who_points = prev;
-    if (curr->size >= asize)
-        return curr;
-    else
-        return NULL;
-}
-
-Header new_block(size_t pasize){
-
-    void* mem = alloc_pages(pasize);
+    void* mem = fl_alloc(pasize);
     if (mem == NULL)
         return NULL;
     Header h = mem;
@@ -251,11 +300,11 @@ Header new_block(size_t pasize){
     return h;
 }
 
-Header split(void* origin, size_t origin_size, size_t asize, int* status, void** points_where){
+Header fl_split_block(void* origin, size_t origin_size, size_t asize, int* status, void** points_where){
 
     Header remainder = NULL;
     Header origin_mod = origin;
-    if (points_where != NULL)
+    if (points_where != NULL) // the user might supply a null param
         *points_where = origin_mod->next;
     origin_mod->size = asize-HEAD_SIZE;
     origin_mod->magic_number = 123456;
@@ -263,7 +312,6 @@ Header split(void* origin, size_t origin_size, size_t asize, int* status, void**
     if (origin_size-asize > 0){
         remainder = origin + asize;
         remainder->size = origin_size-asize;
-        remainder->next = NULL;
         *status = FREE_SPACE_REMAINING;
     } else if (origin_size-asize == 0)
         *status = ALL_FREE_SPACE_FILLED;
@@ -273,12 +321,12 @@ Header split(void* origin, size_t origin_size, size_t asize, int* status, void**
     return remainder;
 }
 
-void* alloc_pages(size_t pasize){
+void* fl_alloc(size_t pasize){
     void* alloc_mem = mem_map(pasize);
     return alloc_mem;
 }
 
-void dealloc_pages(){
+void fl_dealloc(){
     Header prev = NULL;
     Header curr = FreeList.head;
 
@@ -287,7 +335,7 @@ void dealloc_pages(){
 
     while (curr != NULL){
 
-        if ( !(((uintptr_t)curr) & (APAGE_SIZE - 1)) && !( (curr->size+HEAD_SIZE) & (APAGE_SIZE - 1)) ){
+        if ( !(((uintptr_t)curr) & (APAGE_SIZE - 1)) && !( (curr->size+HEAD_SIZE) & (APAGE_SIZE - 1)) ){ // check idea borrowed from mdriver.c
             if (prev != NULL) {
                 prev->next = curr->next;
             } else {
@@ -307,29 +355,63 @@ void dealloc_pages(){
     }
 }
 
-/*
- * mm_free - Freeing a block does nothing.
- */
-void mm_free(void* ptr)
-{
-    /* You must implement this. If a page is completely free, use
-     * mem_unmap() from driver/memlib.c to return it to the OS.
-     */
+void fl_mergesort(Header* head) {
 
-    if (ptr == NULL) {
-        return;
-    }
+    Header h1;
+    Header h2;
 
-    Header ptr_h = ptr-HEAD_SIZE;
-    if (ptr_h == NULL)
+    if (*head == NULL || (*head)->next == NULL)
         return;
 
-    if (ptr_h->magic_number == 123456){
-        ptr_h->next = FreeList.head;
-        FreeList.head = ptr_h;
-        merge_terse();
-        dealloc_pages();
+    fl_mergesort_split(*head, &h1, &h2);
+
+    fl_mergesort(&h1); //recursive sort the 2 halves
+    fl_mergesort(&h2);
+
+    //merge
+    *head = fl_mergesort_merge(h1, h2);
+}
+
+Header fl_mergesort_merge(Header h1, Header h2) {
+    Header to_ret = NULL;
+
+    if (h1 == NULL)
+        return(h2);
+    else if (h2 == NULL)
+        return(h1);
+
+    if (h1->size >= h2->size) { // set for descending order
+        to_ret = h1;
+        to_ret->next = fl_mergesort_merge(h1->next, h2);
     } else {
-        return;
+        to_ret = h2;
+        to_ret->next = fl_mergesort_merge(h1, h2->next);
+    }
+    return(to_ret);
+}
+
+void fl_mergesort_split(Header head, Header *h1, Header *h2) {
+    Header max;
+    Header mid;
+    if ( head==NULL || head->next==NULL ){
+        *h1 = head;
+        *h2 = NULL;
+    }
+    else {
+        mid = head; // mid will be halfway when max is at last element
+        max = head->next;
+
+        while (max != NULL) {
+            max = max->next;
+
+            if (max != NULL) {
+                mid = mid->next;
+                max = max->next;
+            }
+        }
+
+        *h1 = head;
+        *h2 = mid->next;
+        mid->next = NULL;
     }
 }
--- mm.h
+++ mm.h
@@ -4,15 +4,3 @@ extern int mm_init (void);
 extern void *mm_malloc (size_t size);
 extern void mm_free (void *ptr);
 
-// Custom func
-
-typedef struct header* Header;
-
-void* alloc_pages(size_t pasize);
-Header split(void*, size_t , size_t, int*, void**);
-Header new_block(size_t);
-Header find_block(void**, size_t);
-Header merge(Header, Header);
-void merge_terse();
-void dealloc_pages();
-
